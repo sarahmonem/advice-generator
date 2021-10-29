5ee2e3c8d87f5d9757b86bb31ba894d8c854b3a4
sara <sarahamonem@gmail.com>
2021-10-29 08:39:01 +0300

    json changes

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-let cardTitle = document.querySelector('.title')
-let finishdate = new Date(15 )
+let cardTitle = document.querySelector(".title")
+let finishdate = new Date(15)
 let newdate = new Date()
-fetch(" http://localhost:3000/advices").then(function(res) { return res.json()})
-.then( data => {
-   
-//   if(localStorage.getItem('advice')  && newdate.getMinutes() > 0 && newdate.getHours() > 0 ){
-//     cardTitle.textContent = localStorage.getItem('advice')
-//     console.log(newdate.getHours());
-  
-//   }else    {
-    data.forEach(element => {
-        element = data[Math.floor(Math.random()*data.length)];
-         localStorage.setItem('advice' , `${element.content}`)
-       console.log(element.content);
-        cardTitle.textContent = element.content
-       
-    });
-})
+fetch("/db.json")
+  .then(function (res) {
+    return res.json()
+  })
+  .then((data) => {
+    //   if(localStorage.getItem('advice')  && newdate.getMinutes() > 0 && newdate.getHours() > 0 ){
+    //     cardTitle.textContent = localStorage.getItem('advice')
+    //     console.log(newdate.getHours());
 
+    //   }else    {
+    data.forEach((element) => {
+      element = data[Math.floor(Math.random() * data.length)]
+      //  localStorage.setItem('advice' , `${element.content}`)
+      cardTitle.textContent = element.content
+    })
+  })
 
-let dateText = document.querySelector('.date')
+let dateText = document.querySelector(".date")
 setInterval(() => {
-   let date = new Date();
+  let date = new Date()
   dateText.textContent = date.toLocaleTimeString()
-}, 1000);
-
+}, 1000)
